dee19f730a67488dbcbd1248a56d52b21e106f9e
Andressa Bovolenta <andressa.bbovolenta@gmail.com>
2021-03-31 14:22:52 -0300

    First commit

---
+++ ContaBancária.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO1
+{
+    class ContaBancária
+    {
+        public string Agência;
+        public string Conta;
+        public string Nome;
+        private string Senha = "024680";
+        private int Saldo;
+        
+        public ContaBancária(int saldo)
+        {
+            Saldo = saldo;
+        }
+        public void Depositar(int valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public int ConsultarSaldo()
+        {
+            int taxa = 10;
+            int valorTaxa = (Saldo * 10) / 100;
+            return Saldo - valorTaxa;
+        }
+
+        public void ImprimirSaldo()
+        {
+            Console.WriteLine($"{Nome}, o saldo da sua conta {Conta}, agência {Agência} é: R${ConsultarSaldo()}.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancária contaBancária = new ContaBancária(200);
+
+            contaBancária.Agência = "0123";
+            contaBancária.Conta = "456789-0";
+            contaBancária.Nome = "Andressa Bovolenta";
+            contaBancária.Senha = "098765"
+
+            contaBancária.ImprimirSaldo();
+
+        }
+    }
+}
